08f46f2ae86592e3f89fa77238470000f4939211
MusfiqunNoorMonon <159142815+MusfiqunNoorMonon@users.noreply.github.com>
2025-02-13 01:19:00 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let x,y;
+let b=0;
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition;
+
+function listening(){
+  document.getElementById('status').innerHTML="Listening....Speak now!";
+  speak();
+  recognition.start();
+  recognition.onresult=function(event){
+    console.log(event);
+    var a=event.results[0][0].transcript.toLowerCase();
+    document.getElementById("status").innerHTML="You said: "+ a;
+    if (a.includes("circle")) {
+        b=circle;
+        x=prompt("x=?")
+        y=prompt("y=?")
+        radius=prompt("radius=?")
+    } 
+  }
+}
+
+function speak(){
+  var synth=window.speechSynthesis;
+  var utterThis=new SpeechSynthesisUtterance("Tell the shape you want to draw");
+  synth.speak(utterThis);
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  if (b==circle){
+    circle(x,y,radius)
+  }
+}
