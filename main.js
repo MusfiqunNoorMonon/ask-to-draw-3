d400230cf5b0ed417b2d44af5f9e7e47d711b119
MusfiqunNoorMonon <159142815+MusfiqunNoorMonon@users.noreply.github.com>
2025-02-13 02:13:28 +0600

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let x,y;
 let b=0;
+let speechgiven=0;
 var SpeechRecognition=window.webkitSpeechRecognition;
 var recognition=new SpeechRecognition;
 
@@ -12,17 +13,17 @@ function listening(){
     var a=event.results[0][0].transcript.toLowerCase();
     document.getElementById("status").innerHTML="You said: "+ a;
     if (a.includes("circle")) {
-        b=circle;
-        x=prompt("x=?")
-        y=prompt("y=?")
-        radius=prompt("radius=?")
+        b="Circle";
+        x=Number(prompt("x=?"));
+        y=Number(prompt("y=?"));
+        radius=Number(prompt("radius=?"));
     } 
   }
 }
 
 function speak(){
   var synth=window.speechSynthesis;
-  var utterThis=new SpeechSynthesisUtterance("Tell the shape you want to draw");
+  var utterThis=new SpeechSynthesisUtterance("Tell the shape you want to draw and write the values");
   synth.speak(utterThis);
 }
 
@@ -30,11 +31,19 @@ function speak(){
 
 function setup() {
   createCanvas(400, 400);
-  background("purple");
+  background("pink")
 }
 
 function draw() {
-  if (b==circle){
-    circle(x,y,radius)
+  if (b=="Circle"){
+    circle(x,y,radius);
+    b=0;
+    speechgiven=0;
+  }
+  else if (speechgiven==0){
+    var synthI=window.speechSynthesis;
+    var utterThisI=new SpeechSynthesisUtterance("You should Enter a number and you have to tell shape name");
+    synthI.speak(utterThisI);
+    speechgiven=1;
   }
 }
